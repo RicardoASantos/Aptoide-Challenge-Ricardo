bc393176bbddedd80857a3a31c856022a985dfad
RicardoSantos <rdosantos.multimedia@gmail.com>
2018-04-11 01:41:00 +0100

    App

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { NgModule } from '@angular/core'
+import { HttpModule, Http,  XHRBackend, RequestOptions } from '@angular/http'
+import { FileUploadModule } from 'ng2-file-upload'
+
+import { AppComponent } from './app.component'
+import { CKEDITOR } from './ckeditor.component'
+
+import { NewsService } from './news.service'
+
+
+import {ImagePreview} from './image-preview.directive'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImagePreview,
+    CKEDITOR
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FileUploadModule
+  ],
+  providers: [
+    NewsService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
